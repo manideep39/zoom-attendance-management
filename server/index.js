aab28df2de4ad2bd4e8e10c7bf9a8854bc73875f
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2021-05-27 00:43:16 +0530

    added: functionality to upload csv file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
-const { readCSV } = require("./utils/readCSV.js");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-const data = readCSV("data.csv");
+const app = express();
 
-console.log(data);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
+
+app.post("/upload", upload.single("zoomRecord"), (req, res) => {
+  console.log(req.file);
+  res.send("file uploaded");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// const { readCSV } = require("./utils/readCSV.js");
+
+// const data = readCSV("data.csv");
+
+// console.log(data);
