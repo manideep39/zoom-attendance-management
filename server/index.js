e0e0235140f52eda32e78c39bd37662b08f877fd
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2021-05-30 15:10:31 +0530

    correcting api which was changed before to test deployment

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.post(
   }
 );
 
-app.get("/getSummary", (req, res) => {
+app.get("/api/getSummary", (req, res) => {
   const summary = generateSummary();
   res.send(JSON.stringify(summary));
 });
